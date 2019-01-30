6dcfd9c20459655f3407982fd7664fe44b5648db
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-30 16:37:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function breakOut(array, changeValue, stopValue) {
+  array.forEach((element, index) => {
+      if(element == stopValue) {
+        break
+      }
+      array[index] = changeValue
+  })
+}
